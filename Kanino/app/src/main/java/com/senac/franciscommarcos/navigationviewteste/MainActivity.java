236d8402268a6f7be85693065347245476a4bcd4
fmarcos8 <francisco-marcos@hotmail.com>
2017-10-14 21:56:57 -0300

    Product Partial

--- Kanino/app/src/main/java/com/senac/franciscommarcos/navigationviewteste/MainActivity.java
+++ Kanino/app/src/main/java/com/senac/franciscommarcos/navigationviewteste/MainActivity.java
@@ -1,14 +1,34 @@
 package com.senac.franciscommarcos.navigationviewteste;
 
+import java.text.NumberFormat;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView product_picture;
+    private TextView product_name;
+    private TextView product_price;
+    private TextView product_description;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
@@ -21,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        product_name = (TextView) findViewById(R.id.product_name);
+        product_price = (TextView) findViewById(R.id.product_price);
+        product_description = (TextView) findViewById(R.id.product_description);
 
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener(){
                     public boolean onNavigationItemSelected(MenuItem menuItem){
@@ -33,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         drawerLayout.closeDrawers();
-
                         if(menuItem.getItemId() == R.id.action_login){
                             LoginFragment fragment = new LoginFragment();
                             getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, fragment).commit();
@@ -54,6 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+        NetWorkCall myCall = new NetWorkCall();
+        //myCall.execute("http://kanino-pi4.azurewebsites.net/Kanino/api/produtos");
+        myCall.execute("http://kanino-pi4.azurewebsites.net/Kanino/api/produto/8");
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer, R.string.closeDrawer){};
@@ -68,4 +94,50 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+    public class NetWorkCall extends AsyncTask<String, Void, String>{
+        Bitmap bmp = null;
+        @Override
+        protected String doInBackground(String... params) {
+            try{
+                HttpURLConnection urlConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+                urlConnection.setConnectTimeout(4000);
+                InputStream in =  urlConnection.getInputStream();
+                BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(in, "UTF-8"));
+                StringBuilder resultado = new StringBuilder();
+                String linha = bufferedReader.readLine();
+
+
+
+                while(linha != null){
+                    resultado.append(linha);
+                    linha = bufferedReader.readLine();
+                }
+
+                String respostaCompleta = resultado.toString();
+
+                return respostaCompleta;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            try{
+                JSONObject json = new JSONObject(result);
+                String name = json.getString("name");
+                String price = json.getString("price");
+                String description = json.getString("description");
+                product_name.setText(name);
+                product_price.setText(NumberFormat.getCurrencyInstance().format(Double.parseDouble(price)));
+                product_description.setText(description);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
---
+++ Kanino/app/src/main/java/com/senac/franciscommarcos/navigationviewteste/Product.java
@@ -0,0 +1,96 @@
+package com.senac.franciscommarcos.navigationviewteste;
+
+/**
+ * Created by Francisco on 14/10/2017.
+ */
+
+public class Product {
+    private int id;
+    private String name;
+    private String price;
+    private String description;
+    private String discountPromotion;
+    private int activeProduct;
+    private int minimumStockQuantity;
+    private String image;
+
+    public Product(int id, String name, String price, String description, String discountPromotion, int activeProduct, int minimumStockQuantity, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.discountPromotion = discountPromotion;
+        this.activeProduct = activeProduct;
+        this.minimumStockQuantity = minimumStockQuantity;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDiscountPromotion() {
+        return discountPromotion;
+    }
+
+    public void setDiscountPromotion(String discountPromotion) {
+        this.discountPromotion = discountPromotion;
+    }
+
+    public int getActiveProduct() {
+        return activeProduct;
+    }
+
+    public void setActiveProduct(int activeProduct) {
+        this.activeProduct = activeProduct;
+    }
+
+    public int getMinimumStockQuantity() {
+        return minimumStockQuantity;
+    }
+
+    public void setMinimumStockQuantity(int minimumStockQuantity) {
+        this.minimumStockQuantity = minimumStockQuantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String toString() {
+        return "Id: "+ getId() +"Produto: " + getName() + " Descrição: " +
+                getDescription() + " preco: " + getPrice();
+    }
+}
